19156ee1142db72858a8cc81a93e1bb4d661833c
laohur <laohur@gmail.com>
2022-06-17 00:47:38 +0800

    extract_all.py same languages

--- extract_all.py
+++ extract_all.py
@@ -21,7 +21,7 @@ from logzero import logger
 from extractor import extract_wiki
 
 
-def parse_all(src_dir, tgt_dir, lang="*", compress_type="xz"):
+def parse_all(src_dir, tgt_dir, lang="*", compress_type="xz", same_language=False):
     srcs = glob.glob(rf"{src_dir}/{lang}*.gz")
     srcs = list(srcs)
     srcs.sort()
@@ -30,11 +30,12 @@ def parse_all(src_dir, tgt_dir, lang="*", compress_type="xz"):
         t = name.rstrip(".json.gz")
         if t[:2] <= 'a ':
             continue
-        tgt = f"{tgt_dir}/{t}.txt"
-        extract_wiki(src, tgt, compress_type)
+        tgt = f"{tgt_dir}/{t}.txt.{compress_type}"
+        extract_wiki(src, tgt, compress_type, same_language)
         # break
+
 
 if __name__ == '__main__':
     src_dir = "F:/data/wiki-20220606-cirrussearch-content-json-gz"
     tgt_dir = "F:/data/wiki-20220606-cirrussearch-content-txt-bz2"
-    parse_all(src_dir, tgt_dir, compress_type="bz2")
+    parse_all(src_dir, tgt_dir, compress_type="bz2", same_language=True)
--- extractor.py
+++ extractor.py
@@ -27,7 +27,7 @@ def parse_line(a, b):
     content = json.loads(b)
     type = index['index']['_type']
     # id = index['index']['_id']
-    # language = content['language']
+    language = content['language']
     # revision = content['version']
     if type == 'page' and content['namespace'] == 0:
         # title = content['title'].strip()   # nosense
@@ -42,10 +42,10 @@ def parse_line(a, b):
         #     id, url, title, language, revision)
         # page = header + title + '\n\n' + text + '\n</doc>\n'
         page = text.strip()
-    return page
+    return page, language
 
 
-def process_dump(input_file, out_file, compress_type=""):
+def process_dump(input_file, out_file, compress_type="", same_language=False):
     """
     :param input_file: name of the wikipedia dump file; '-' to read from stdin
     :param out_file: directory where to store extracted data, or '-' for stdout
@@ -64,9 +64,9 @@ def process_dump(input_file, out_file, compress_type=""):
         if not compress_type:
             output = open(out_file, "w")
         elif compress_type == "bz2":
-            output = bz2.BZ2File(out_file + '.bz2', 'w')
+            output = bz2.BZ2File(out_file, 'w')
         elif compress_type == "xz":
-            output = lzma.open(out_file+'.xz', "w")
+            output = lzma.open(out_file, "w")
         else:
             raise ValueError("invalid compress_type "+compress_type)
 
@@ -74,7 +74,8 @@ def process_dump(input_file, out_file, compress_type=""):
     # format
     # {"index":{"_type":"page","_id":"3825914"}}
     # {"namespace":0,"title":TITLE,"timestamp":"2014-06-29T15:51:09Z","text":TEXT,...}
-    n_line = 0
+    n_src = 0
+    n_tgt = 0
     while True:
         line = input.readline()
         if not line:
@@ -82,36 +83,49 @@ def process_dump(input_file, out_file, compress_type=""):
         if not output:
             continue
         doc = input.readline()
-        n_line += 2
+        n_src += 2
         try:
-            page = parse_line(line, doc)
+            page, language = parse_line(line, doc)
             if not page:
                 continue
-            page+='\n'
+            name = os.path.basename(input_file)
+            if same_language and language[:2] != name[:2]:
+                logger.warning(
+                    f"{ language[:2]}!={input_file[:2]} ignore {page[:100]} ")
+                continue
+            page += '\n'
             if compress_type:
-                page=page.encode('utf-8')
+                page = page.encode('utf-8')
             output.write(page)
+            n_tgt += 1
         except Exception as e:
             logger.error(e)
-    return n_line
+    return n_src, n_tgt
 
 
-def extract_wiki(src, tgt, compress_type=""):
+def extract_wiki(src, tgt, compress_type="", same_language=False):
     if os.path.exists(tgt):
         # os.remove(tgt)
         logger.warning(f" -->  {tgt}  exists!")
         return
     logger.info(f"{src}  -->  ...")
     try:
-        n_line = process_dump(src, tgt, compress_type)
-        logger.info(f" n_line:{n_line} -->  {tgt} ")
+        n_src, n_tgt = process_dump(src, tgt, compress_type,
+                                    same_language=same_language)
+        logger.info(f" n_src:{n_src} --> n_tgt：{n_tgt} {tgt} ")
+        if not n_tgt:
+            os.remove(tgt)
+            logger.warning(f" --> empty {tgt}  cleaned!")
+            return
     except Exception as e:
         logger.error(e)
         if os.path.exists(tgt):
             os.remove(tgt)
             logger.error(f" -->  {tgt}  cleaned!")
             return
+
     return tgt
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -119,6 +133,8 @@ if __name__ == '__main__':
                         help="Cirrus Json wiki dump file")
     parser.add_argument("--tgt", default="-")
     parser.add_argument("--compress_type", default="")
+    parser.add_argument("--same_language", default=False)
 
     args = parser.parse_args()
-    extract_wiki(args.src, args.tgt, args.compress_type)
+    extract_wiki(args.src, args.tgt, args.compress_type,
+                 same_language=args.same_language)
